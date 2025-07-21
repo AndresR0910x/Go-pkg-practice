750876e4f8cfef98f571e36d7936532b9ce5f306
AndresR0910x <androd2000x@gmail.com>
2025-07-21 15:18:08 -0500

    Funciones agregadas

--- godesde0/main.go
+++ godesde0/main.go
@@ -1,11 +1,14 @@
-
 package main
 
-
 import (
+	"fmt"
+
 	"github.com/MrEngineer/godesde0/variables"
 )
 
 func main() {
-	variables.ShowInteger()
-}
+	estado, texto := variables.ConvierteATexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
+	
+}	
---
+++ godesde0/variables/resto.go
@@ -0,0 +1,32 @@
+package variables
+
+import (
+	"fmt"
+	"time"
+	"strconv"
+)
+
+//SCOPE
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time
+
+
+
+func RestoVariables() {
+	Nombre = "Pedro"
+	Estado = true
+	Sueldo = 1577.66
+	Fecha = time.Now()
+	fmt.Println(Nombre)
+	fmt.Println(Estado)
+	fmt.Println(Sueldo)
+	fmt.Println(Fecha)
+
+}
+
+func ConvierteATexto(number int) (bool, string) {
+	text := strconv.Itoa(number)
+	return true, text
+}
