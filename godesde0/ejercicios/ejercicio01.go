f50720e07f56141236379d256b90046648aa795f
AndresR0910x <androd2000x@gmail.com>
2025-07-21 16:33:04 -0500

    Siguiente commit

---
+++ godesde0/ejercicios/ejercicio01.go
@@ -0,0 +1,19 @@
+package ejercicios
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func ConvertStirng (text string) (int, string) {
+	entero, error := strconv.Atoi(text)
+	if error != nil {
+		fmt.Println("Error al convertir el texto a entero:", error)
+	} else if entero > 100 {
+		fmt.Println("El número es mayor que 100")
+	} else {
+		fmt.Println("El número es menor o igual a 100")
+	}
+
+	return entero, text
+}
--- godesde0/main.go
+++ godesde0/main.go
@@ -3,12 +3,29 @@ package main
 import (
 	"fmt"
 
-	"github.com/MrEngineer/godesde0/variables"
+	"github.com/MrEngineer/godesde0/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConvierteATexto(1588)
+	/**estado, texto := variables.ConvierteATexto(1588)
 	fmt.Println(estado)
 	fmt.Println(texto)
-	
-}	
+	if os := runtime.GOOS; os == "windows" {
+		fmt.Println("Ejecutando en Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		println("Ejecutando en Linux")
+	case "darwin":
+		println("Ejecutando en MacOS")
+	default:
+		fmt.Printf("%s \n", os)
+
+	}*/
+	numero, texto := ejercicios.ConvertStirng("Andresito")
+
+	fmt.Println("Número convertido:", numero)
+	fmt.Println("Texto original:", texto)
+
+}
